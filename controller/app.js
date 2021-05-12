cfbfb2d8455baae3d4cad04af539e318181a8d29
mor yaacov <mory1996@gmail.com>
2021-05-12 23:08:06 +0300

    finally managed to send a request to server!! and the design is also working on port 8080

--- controller/app.js
+++ controller/app.js
@@ -3,18 +3,19 @@ const fs = require ('fs')
 const fileUpload = require('express-fileupload')
 const bodyParser = require('body-parser');
 const app = express()
-
 app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 8080;
+
 app.use(express.urlencoded({
     extended: false
 }))
+app.use(express.static("web_ui"));
 
- app.get("/", (req,res)=> {
-    res.sendFile('./main_window.html', { root: './web_ui' });
-   //  res.sendFile('asa.html', { root: __dirname });
+app.get("/", (req,res)=> {
+    res.sendFile('./index.html', { root: './web_ui' });
+    // res.sendFile('asa.html', { root: __dirname });
 });
 
 app.post('/api/detect', (req,res) => {
@@ -25,6 +26,9 @@ app.post('/api/detect', (req,res) => {
     if (model_type !== "regression" && model_type !== "hybrid") {
         res.status(400).send("unsupported model type")
     }
+
+    if(req.files)
+        console.log(req.files);
 
     let model = req.files.model;
     let anomaly = req.files.anomaly;
