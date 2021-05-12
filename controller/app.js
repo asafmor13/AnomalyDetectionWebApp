7dc90b905b5d14b38603140de2772ff013b88720
asaf mor <asafmor13@gmail.com>
2021-05-12 21:03:54 +0300

    fixed post request

--- controller/app.js
+++ controller/app.js
@@ -8,23 +8,24 @@ app.use(fileUpload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const port = 8080;
-
+app.use(express.urlencoded({
+    extended: false
+}))
 
  app.get("/", (req,res)=> {
-    //res.sendFile('./main_window.html', { root: './web_ui' });
-     res.sendFile('asa.html', { root: __dirname });
+    res.sendFile('./main_window.html', { root: './web_ui' });
+   //  res.sendFile('asa.html', { root: __dirname });
 });
 
 app.post('/api/detect', (req,res) => {
     if(!req.query.model_type){
-        res.status(400).send("no")
+        res.status(400).send("missing model type")
     }
     let model_type = req.query.model_type;
     if (model_type !== "regression" && model_type !== "hybrid") {
-        res.status(400).send("no")
+        res.status(400).send("unsupported model type")
     }
-    if(req.files)
-        console.log(req.files)
+
     let model = req.files.model;
     let anomaly = req.files.anomaly;
 
